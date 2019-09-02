e4f21f2539b4f921b2b1c51c6132a9aaa5b8c904
mirceah <mircea.hasegan@gmail.com>
2019-09-02 11:43:21 +0300

    styling product details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,6 @@ export class AppComponent  {
     name: 'iPhone L',
     summary: 'iPhone L(50) 2030',
     description: 'The new and amazing iPhone L (or 50) blows the minds of users with right to left gestures instead of old and out of fashion bottom to up',
-    price: '5000$',
+    price: '5000',
   };
 }
